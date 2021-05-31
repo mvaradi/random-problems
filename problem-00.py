92f3f69b12d268d7a8abdef01db84118d25b9f60
mvaradi <mvaradi@ebi.ac.uk>
2021-05-31 21:40:24 +0100

    Typo

--- problem-00.py
+++ problem-00.py
@@ -1,5 +1,5 @@
 """
-PROBLEM 01 - Counting DNA Nucleotides
+PROBLEM 00 - Counting DNA Nucleotides
 
 A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a
 string is the number of symbols that it contains.
