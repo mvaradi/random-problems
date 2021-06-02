e1dbd84abceec4dfbda4df5b832c9c36d94f981c
Mihaly Varadi <mvaradi@ebi.ac.uk>
2021-06-02 17:51:42 +0100

    Refactoring the file handling

--- rosalind/file_handling.py
+++ rosalind/file_handling.py
@@ -1,11 +1,39 @@
-def load_sequence(path):
+class FileHandler():
     """
-    Simple function to get a sequence from a file that only has one line
-    :param path: String; path to the file
-    :return: String; a sequence
+    This class handles input files in various formats
     """
-    try:
-        with open(path) as sequence_file:
-            return sequence_file.readline().strip()
-    except IOError:
-        raise Exception('File error')
+
+    def __init__(self, path):
+         self.path = path
+         self.input_file = None
+         self.data = None
+
+    def __repr__(self):
+        return f'Opened {self.path}'
+
+    def _open_file(self):
+        """"
+        Open a file or raise error
+        :return: None;
+        """
+        try:
+            self.input_file = open(self.path)
+        except IOError:
+            raise Exception('File error')
+
+    def _close_file(self):
+        """
+        Close a file
+        """
+        self.input_file.close()
+
+    def load_sequence(self):
+        """
+        Simple function to get a sequence from a file that only has one line
+        :param path: String; path to the file
+        :return: String; a sequence
+        """
+        self._open_file()
+        data = self.input_file.readline().strip()
+        self._close_file()
+        return data
--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,19 +1,19 @@
 import os
 from unittest import TestCase
-from rosalind.file_handling import load_sequence
+from rosalind.file_handling import FileHandler
 
 
-class TestLoadSequence(TestCase):
+class TestFileHandler(TestCase):
     """
     Test the file handling functions
     """
 
     def test_load_sequence(self):
         os.system('echo FOO > tmp.txt')
-        seq = load_sequence('tmp.txt')
+        seq = FileHandler('tmp.txt').load_sequence()
         self.assertEqual(seq, 'FOO')
         os.system('rm tmp.txt')
 
     def test_load_sequence_no_file(self):
         with self.assertRaises(Exception) as context:
-            load_sequence('missing.txt')
+            FileHandler('missing.txt').load_sequence()
